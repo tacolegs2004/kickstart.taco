14e85cbe8859b9b687b7c1909db6efee7066ee5a
taco boi <tylerchall16@gmail.com>
2024-02-11 22:53:37 -0600

    initial commit

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+	},
+	{ "craftzdog/solarized-osaka.nvim" },
+	{ "shaunsingh/oxocarbon.nvim" },
+	{ "ellisonleao/gruvbox.nvim" },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine"
+	},
+	{ "ellisonleao/gruvbox.nvim" },
+}
+
+-- require("plugins.config.catppuccin")
