ff45518289215b1912814edf724e5d70eaf20c49
taco boi <tylerchall16@gmail.com>
2024-09-25 22:51:17 -0500

    added js, jsx, ts, and tsx to treesitter ensure_installed, working on neorg

--- init.lua
+++ init.lua
@@ -128,6 +128,30 @@ require('lazy').setup({
       vim.cmd([[colorscheme catppuccin-mocha]])
     end
   },
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    version = "*",
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/Documents/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
   'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
@@ -153,37 +177,43 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
-  {
-    "goolord/alpha-nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-
-    config = function()
-      local alpha = require("alpha")
-      local dashboard = require("alpha.themes.startify")
-
-      dashboard.section.header.val = {
-        [[                                                                       ]],
-        [[                                                                       ]],
-        [[                                                                       ]],
-        [[                                                                       ]],
-        [[                                                                     ]],
-        [[       ████ ██████           █████      ██                     ]],
-        [[      ███████████             █████                             ]],
-        [[      █████████ ███████████████████ ███   ███████████   ]],
-        [[     █████████  ███    █████████████ █████ ██████████████   ]],
-        [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-        [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-        [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-        [[                                                                       ]],
-        [[                                                                       ]],
-        [[                                                                       ]],
-      }
-
-      alpha.setup(dashboard.opts)
-    end,
-  },
+  -- {
+  -- "goolord/alpha-nvim",
+  -- dependencies = {
+  --   "nvim-tree/nvim-web-devicons",
+  -- },
+  --
+  -- config = function()
+  --   local alpha = require("alpha")
+  --   local dashboard = require("alpha.themes.startify")
+  --
+  --   dashboard.section.header.val = {
+  --     '',
+  --     '',
+  --     '888b    888                  888     888 d8b               ',
+  --     '8888b   888                  888     888 Y8P               ',
+  --     '88888b  888                  888     888                   ',
+  --     '888Y88b 888  .d88b.   .d88b. Y88b   d88P 888 88888b.d88b.  ',
+  --     '888 Y88b888 d8P  Y8b d88""88b Y88b d88P  888 888 "888 "88b ',
+  --     '888  Y88888 88888888 888  888  Y88o88P   888 888  888  888 ',
+  --     '888   Y8888 Y8b.     Y88..88P   Y888P    888 888  888  888 ',
+  --     '888    Y888  "Y8888   "Y88P"     Y8P     888 888  888  888 ',
+  --   }
+  --
+  --   dashboard.section.buttons.val = {
+  --     dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+  --     dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+  --     dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+  --     dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+  --     dashboard.button("m", "  BookMarks", ":Telescope marks <CR>"),
+  --     dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+  --     dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua"),
+  --     dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+  --   }
+  --
+  --   alpha.setup(dashboard.opts)
+  -- end,
+  -- },
   {
     "folke/trouble.nvim",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
@@ -382,6 +412,9 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    opts = {
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "javascriptreact", "typescript", "typescriptreact" }
     },
     build = ':TSUpdate',
   },
